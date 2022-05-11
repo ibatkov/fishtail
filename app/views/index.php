e0014577404a17bb61b0d491668831c54d3a1688
ibatkov <invakersapurt@gmail.com>
2022-05-12 00:40:13 +0300

    added logo. Added bootstrap. Added index template.

---
+++ app/views/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fish Tail</title>
+    <link href="views/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .bg-header {
+            background-color: rgba(107, 153, 198,1) !important;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-7 py-4">
+                    <h4 class="text-white">Про нас</h4>
+                    <p class="text-muted"><strong>Fish tail</strong> - это простой сокращатель ссылок, предоставляющий вам возможность не
+                        только уменьшить размер большой ссылки, но и просматривать статистику переходов по ним</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar navbar-dark bg-header shadow-sm">
+        <div class="container">
+            <a href="#" class="navbar-brand d-flex align-items-center">
+                <img src="views/img/logo.svg" width="30" height="30" alt="FishTail">
+                <div style="font-size: 1.5rem"><strong>FishTail</strong></div>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
+                    aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+</header>
+
+<script src="views/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
